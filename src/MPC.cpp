5c8ec6df8e3bff6b305a90ade792e02d997aa5ae
Alper <dcs1986@gmail.com>
2018-04-06 23:36:29 +0300

    Added writeup

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -105,7 +105,7 @@ class FG_eval {
 		  
 		  AD<double> delta0 = vars[delta_start + t - 1];
 		  
-		  AD<double> f0 = coeffs[0] + coeffs[1] * x0;
+		  AD<double> f0 = coeffs[0] + coeffs[1] * x0 + coeffs[2]*x0*x0;
 		  AD<double> psides0 = CppAD::atan(coeffs[1]);
 		  // Here's `x` to get you started.
 		  // The idea here is to constraint this value to be 0.
